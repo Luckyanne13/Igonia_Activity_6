96f6aa6c3c6d28a99eeca81bc054521178b1f6fa
julia <juliannedeniseigonia18@gmail.com>
2024-06-05 15:57:27 -0800

    Initial commit

---
+++ app/src/main/java/com/example/buildapp/Data/DataSource.kt
@@ -0,0 +1,27 @@
+package com.example.buildapp.Data
+
+import com.example.buildapp.R
+import com.example.buildapp.model.Topic
+
+
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.menu_1,availableCourses = 58,R.drawable.menu_1),
+        Topic(R.string.menu_2,availableCourses = 58,R.drawable.menu_2),
+        Topic(R.string.menu_3,availableCourses = 58,R.drawable.menu_3),
+        Topic(R.string.menu_4,availableCourses = 58,R.drawable.menu_4),
+        Topic(R.string.menu_5,availableCourses = 58,R.drawable.menu_5),
+        Topic(R.string.menu_6,availableCourses = 58,R.drawable.menu_6),
+        Topic(R.string.menu_7,availableCourses = 58,R.drawable.menu_7),
+        Topic(R.string.menu_8,availableCourses = 58,R.drawable.menu_8),
+        Topic(R.string.menu_9,availableCourses = 58,R.drawable.menu_9),
+
+    )
+    }
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/buildapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.buildapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import com.example.buildapp.ui.theme.BuildAppTheme
+import androidx.compose.material.*
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BuildAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TopicGrid()
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
